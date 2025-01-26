7c874dca973b31217aab760690bf5134ddb47f3a
Harsh Sandilya <harshsandilya09@gmail.com>
2025-01-26 13:56:41 +0530

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para" style={{ textAlign: "justify" }}> 
+        Results-driven technology professional with a specialization in cybersecurity research, threat intelligence,
+ and security auditing & consulting. Expert in WordPress development, UI/UX design, digital marketing
+ optimization, and advanced SEO methodologies. Skilled in orchestrating complex projects, leading cross
+functional teams, and integrating cutting-edge technologies to drive innovation and growth. Successfully
+ trained over 1000+ individuals, fostering technical proficiency and industry readiness. Committed to
+ delivering high-impact solutions that align with evolving tech landscapes and business objectives
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/data/work.js
@@ -0,0 +1,45 @@
+
+export const WorkData = [
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+    {
+        name:"E Commerce",
+        category:"UI/UX",
+        image:"",
+        tools:"Figma, Adobe",
+
+    },
+]
