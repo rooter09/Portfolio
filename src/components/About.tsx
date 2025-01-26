99a90d7b465c5ff365ab45a8a68699ae13c4344e
Harsh Sandilya <harshsandilya09@gmail.com>
2025-01-26 15:10:23 +0530

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,13 +5,27 @@ const About = () => {
     <div className="about-section" id="about">
       <div className="about-me">
         <h3 className="title">About Me</h3>
-        <p className="para" style={{ textAlign: "justify" }}> 
-        Results-driven technology professional with a specialization in cybersecurity research, threat intelligence,
- and security auditing & consulting. Expert in WordPress development, UI/UX design, digital marketing
- optimization, and advanced SEO methodologies. Skilled in orchestrating complex projects, leading cross
-functional teams, and integrating cutting-edge technologies to drive innovation and growth. Successfully
- trained over 1000+ individuals, fostering technical proficiency and industry readiness. Committed to
- delivering high-impact solutions that align with evolving tech landscapes and business objectives
+        <p
+          className="para"
+          style={{
+            textAlign: "justify",
+            fontFamily: "Montserrat, sans-serif",
+            color: "#555",
+            width: "100%",
+            margin: "0 auto",
+            transition: "color 0.3s ease" 
+          }}
+        >
+          Results-driven technology professional with a specialization in
+          cybersecurity research, threat intelligence, and security auditing &
+          consulting. Expert in WordPress development, UI/UX design, digital
+          marketing optimization, and advanced SEO methodologies. Skilled in
+          orchestrating complex projects, leading cross-functional teams, and
+          integrating cutting-edge technologies to drive innovation and growth.
+          Successfully trained over 1000+ individuals, fostering technical
+          proficiency and industry readiness. Committed to delivering
+          high-impact solutions that align with evolving tech landscapes and
+          business objectives.
         </p>
       </div>
     </div>
