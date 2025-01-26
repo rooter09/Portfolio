8a3c83061a1e572e1a9e174a1460e22ba079614d
Harsh Sandilya <harshsandilya09@gmail.com>
2025-01-26 14:09:46 +0530

    update

--- src/components/utils/initialFX.ts
+++ src/components/utils/initialFX.ts
@@ -1,10 +1,10 @@
 import { SplitText } from "gsap-trial/SplitText";
 import gsap from "gsap";
-import { smoother } from "../Navbar";
+// import { smoother } from "../Navbar";
 
 export function initialFX() {
   document.body.style.overflowY = "auto";
-  smoother.paused(false);
+  // smoother.paused(false);
   document.getElementsByTagName("main")[0].classList.add("main-active");
   gsap.to("body", {
     backgroundColor: "#0b080c",
