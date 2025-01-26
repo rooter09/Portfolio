1186c439da4bfd6518e4959b00aa6056606bcb1c
Harsh Sandilya <harshsandilya09@gmail.com>
2025-01-27 00:37:27 +0530

    update

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -19,7 +19,6 @@ const Career = () => {
                 <h5>NEXXIGITAL</h5>
               </div>
               <h3>2024-Present</h3>
-
             </div>
             <p>
               As Co-Founder and CTO, I lead product innovation, manage technical
@@ -36,6 +35,22 @@ const Career = () => {
               <h3>2022-2024</h3>
             </div>
             <p>
+              As a Digital Marketing Executive at Purezza, I developed and
+              implemented effective digital marketing strategies, focusing on
+              SEO, content creation, and social media campaigns to drive brand
+              awareness, increase traffic, and generate leads. My efforts
+              contributed to the company's overall marketing success and growth.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Cyber Security Analyst</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
               labore sit non ipsum temporibus quidem, deserunt eaque officiis
               mollitia ratione suscipit repellat.
@@ -44,10 +59,148 @@ const Career = () => {
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
-                <h4>Position In Company</h4>
-                <h5>Company Name</h5>
+                <h4>Threat Hunting</h4>
+                <h5>Purezza Technologies</h5>
               </div>
-              <h3>NOW</h3>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div><div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Project Manager</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div><div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Team Manager</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>WordPress Developer</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Graphics Designer</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Workshops and Training</h4>
+                <h5>Purezza Technologies</h5>
+              </div>
+              <h3>2022-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Senior Graphics Designer</h4>
+                <h5>Clinical AI ASSISTANCE</h5>
+              </div>
+              <h3>2024-2024</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Visual Designer</h4>
+                <h5>Timeline Investment</h5>
+              </div>
+              <h3>2023-2023</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>HOD - Graphics Desgining</h4>
+                <h5>Nyayashaya Foundation</h5>
+              </div>
+              <h3>2023-2023</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Website Developer</h4>
+                <h5>Lush Association</h5>
+              </div>
+              <h3>2023-2023</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+              labore sit non ipsum temporibus quidem, deserunt eaque officiis
+              mollitia ratione suscipit repellat.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Graphics Designer</h4>
+                <h5>Lush Assocation</h5>
+              </div>
+              <h3>Mar 2023-Aug 2023</h3>
             </div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
