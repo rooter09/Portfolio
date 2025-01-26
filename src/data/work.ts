334863e060b075a4fbe1a22fab2543c809611e2c
Harsh Sandilya <harshsandilya09@gmail.com>
2025-01-26 14:01:42 +0530

    update

---
+++ src/data/work.ts
@@ -0,0 +1,46 @@
+type WorkItem = {
+    name: string;
+    category: string;
+    image: string;
+    tools: string;
+  };
+  
+  export const WorkData: WorkItem[] = [
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+    {
+      name: "E Commerce",
+      category: "UI/UX",
+      image: "",
+      tools: "Figma, Adobe",
+    },
+  ];
+  
