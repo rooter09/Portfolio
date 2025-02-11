47cede77e5175042ff39be84544c258268fd18a5
Harsh Sandilya <harshsandilya09@gmail.com>
2025-02-11 13:39:39 +0530

    update

--- src/components/Career.tsx
+++ src/components/Career.tsx
@@ -107,9 +107,9 @@ const Career = () => {
               <h3>Jan 2023- Jan 2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Experienced WordPress Developer specializing in custom themes,
+              plugins, SEO, and performance optimization, delivering
+              high-quality, scalable, and user-friendly websites.
             </p>
           </div>
           <div className="career-info-box">
@@ -121,9 +121,10 @@ const Career = () => {
               <h3>Jan 2023- Jan 2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Designed social media posts, logos, and UI/UX prototypes using
+              Figma and Photoshop. Led branding projects, mentored the design
+              team, and delivered innovative visual solutions for remote clients
+              at Purezza Technologies.
             </p>
           </div>
           <div className="career-info-box">
@@ -135,9 +136,10 @@ const Career = () => {
               <h3>Jan 2023- Jan 2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Conducted workshops on cybersecurity, digital marketing, WordPress
+              development, SEO, and graphic design. Provided hands-on training
+              and mentorship to interns, ensuring skill development and industry
+              readiness through practical learning and real-world applications.
             </p>
           </div>
           <div className="career-info-box">
@@ -149,9 +151,10 @@ const Career = () => {
               <h3>Jan 2024- Feb 2025</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Senior Graphics Designer at Clinical AI Assistance, creating UI/UX
+              prototypes, branding assets, and marketing creatives. Led design
+              team, ensuring impactful visuals, enhancing user engagement, and
+              delivering innovative solutions for healthcare technology.
             </p>
           </div>
           <div className="career-info-box">
@@ -163,23 +166,25 @@ const Career = () => {
               <h3>Aug 2023- Nov 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              Visual Designer at Timeline Investment, crafted engaging branding
+              materials, UI/UX designs, and marketing assets. Enhanced visual
+              identity, ensuring consistency and creativity to elevate the
+              company’s digital presence and client engagement.
             </p>
           </div>
           <div className="career-info-box">
             <div className="career-info-in">
               <div className="career-role">
                 <h4>HOD - Graphics Desgining</h4>
-                <h5>Nyayashaya Foundation</h5>
+                <h5> </h5>
               </div>
               <h3>Aug 2023- Sep 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              As Head of Department - Graphics Designing at Nyayashaya
+              Foundation, led the design team, crafted compelling visuals, and
+              developed branding strategies to amplify outreach and advocacy for
+              social causes.
             </p>
           </div>
           <div className="career-info-box">
@@ -191,9 +196,10 @@ const Career = () => {
               <h3>Mar 2023- Aug 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              As a Website Developer at Lush Association, designed and developed
+              dynamic, user-friendly websites, ensuring seamless functionality,
+              responsive design, and optimized performance to enhance user
+              experience and brand presence.
             </p>
           </div>
           <div className="career-info-box">
@@ -205,9 +211,10 @@ const Career = () => {
               <h3>Mar 2023-Aug 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              As a Graphics Designer at Lush Association, created visually
+              compelling designs, branding assets, and marketing materials,
+              ensuring consistency and engagement across digital and print
+              platforms to enhance brand identity.
             </p>
           </div>
           <div className="career-info-box">
@@ -219,9 +226,10 @@ const Career = () => {
               <h3>Mar 2023-Aug 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              At DE CASTRO GROUP, I worked as a Digital Marketing Intern,
+              optimizing SEO, managing social media campaigns, and creating
+              engaging content to boost brand visibility, drive traffic, and
+              enhance audience engagement across digital platforms.
             </p>
           </div>
           <div className="career-info-box">
@@ -233,9 +241,10 @@ const Career = () => {
               <h3>Mar 2023-Aug 2023</h3>
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-              labore sit non ipsum temporibus quidem, deserunt eaque officiis
-              mollitia ratione suscipit repellat.
+              At Hamari Pahchan, I contributed to social media campaigns,
+              content writing, project management, and strategic research,
+              enhancing team efficiency and improving overall organizational
+              functioning through impactful strategies and mentorship.
             </p>
           </div>
         </div>
